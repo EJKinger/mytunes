d588bcc800885d2ebdd1b2f1b2e8804f8d2ddb11
Eric King <e.king117@gmail.com>
2015-07-15 22:50:11 -0400

    Plays first item in queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,22 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    //set an event listener if something is at position 0
+    //trigger playFirst
+    this.on('add', function(song){
+      console.log('Added to Song Queue');
+      debugger;
+      this.playFirst();
+
+    }, this);
+  },
+  playFirst: function(song){
+
+    return this.models[0].play();
+    //return first item in queue
+    //this.model.play();  
   }
 
 });
+
+//[{artist: aaliya, }]
